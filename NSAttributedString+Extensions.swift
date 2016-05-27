5548329b0cfc8a7235ad8427d0ecda4f082d14da
Vladimir Kazantsev <couriere@yandex.ru>
2016-05-27 10:01:15 +0300

    Method for inserting image in attributed.

--- NSAttributedString+Extensions.swift
+++ NSAttributedString+Extensions.swift
@@ -7,6 +7,27 @@
 
 import UIKit
 
+extension NSMutableAttributedString {
+	
+	/// Вставляет изображение в строку и устанавливает его
+	/// вертикально в центр строки.
+	func insertImage( image: UIImage, atLocation location: Int ) {
+		
+		let textAttachment = NSTextAttachment()
+		textAttachment.image = image
+		let attrStringWithImage = NSAttributedString( attachment:textAttachment )
+		
+		let font = attribute( NSFontAttributeName, atIndex: location, effectiveRange: nil ) as? UIFont ?? UIFont.systemFontOfSize( UIFont.systemFontSize() )
+		let mid = font.descender + font.capHeight
+		textAttachment.bounds = CGRectIntegral(
+			CGRect( x: 0, y: font.descender - image.size.height / 2 + mid + 2,
+				width: image.size.width, height: image.size.height ))
+		
+		self.replaceCharactersInRange( NSMakeRange( location, 0 ), withAttributedString: attrStringWithImage )
+	}
+}
+
+
 extension NSAttributedString {
 	
 	func color( color: UIColor ) -> NSAttributedString {
