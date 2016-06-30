5e870f0f9cc7ee9e7129a182e1d786af8184e801
Vladimir Kazantsev <couriere@yandex.ru>
2016-06-30 15:25:33 +0300

    Method name adjusted to Swift 3

--- UIView+Constraints.swift
+++ UIView+Constraints.swift
@@ -155,10 +155,10 @@ extension UIView {
 		- parameter constant: Constant of a newly created constraint. Defaults to zero.
 		- parameter priority: Priority of a newly created constraint. Defaults to `Required`
 	*/
-	func alignAttribute( attribute: NSLayoutAttribute, withView view: UIView,
-	                     viewAttribute: NSLayoutAttribute? = nil,
-	                     relation: NSLayoutRelation = .Equal,
-	                     constant: CGFloat = 0, priority: UILayoutPriority = 1000 ) -> NSLayoutConstraint {
+	func align( attribute attribute: NSLayoutAttribute, withView view: UIView,
+	                      viewAttribute: NSLayoutAttribute? = nil,
+	                      relation: NSLayoutRelation = .Equal,
+	                      constant: CGFloat = 0, priority: UILayoutPriority = 1000 ) -> NSLayoutConstraint {
 		
 		let constraint = NSLayoutConstraint( item: self, attribute: attribute, relatedBy: relation,
 		                                     toItem: view, attribute: viewAttribute ?? attribute, multiplier: 1, constant: constant )
