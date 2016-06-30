2f204969222b6c955c55630a43b5ca54c58fc580
Vladimir Kazantsev <couriere@yandex.ru>
2016-06-30 15:26:34 +0300

    UIColor constructor from hex string now also accept alpha value.

--- UIColor+Inits.swift
+++ UIColor+Inits.swift
@@ -15,14 +15,14 @@ extension UIColor {
 	
 	/// Returns a UIColor by scanning the string for a hex number.
 	/// Skips any leading whitespace and ignores any trailing characters.
-	convenience init?( hex: String ) {
+	convenience init?( hex: String, alpha: CGFloat = 1 ) {
 
 		guard hex.length == 6 else { return nil }
 		let scanner = NSScanner( string: hex )
 		var hexNum: UInt32 = 0
 		
 		guard scanner.scanHexInt( &hexNum ) else { return nil }
-		self.init( int: hexNum )
+		self.init( int: hexNum, alpha: alpha )
 	}
 
 	/// Returns random color with supplied alpha.
@@ -34,13 +34,13 @@ extension UIColor {
 	}
 	
 	
-	private convenience init( int: UInt32 ) {
+	private convenience init( int: UInt32, alpha: CGFloat = 1 ) {
 		
 		let r = CGFloat(( int >> 16 ) & 0xFF )
 		let g = CGFloat(( int >> 8 ) & 0xFF )
 		let b = CGFloat( int & 0xFF )
 		
-		self.init( red: r / 255, green: g / 255, blue: b / 255, alpha: 1.0 )
+		self.init( red: r / 255, green: g / 255, blue: b / 255, alpha: alpha )
 	}
 	
 }
