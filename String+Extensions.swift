469a701e8d04b622eec574603ccc873796279d65
Vladimir Kazantsev <couriere@yandex.ru>
2016-09-06 14:31:52 +0300

    Update String extensions. Preparing for Swift 3.0

--- String+Extensions.swift
+++ String+Extensions.swift
@@ -10,38 +10,29 @@ import Foundation
 extension String {
 	
 	var length: Int { return self.characters.count }
-
-	func rangeFromNSRange( range: NSRange ) -> Range<String.Index> {
-		let startIndex = self.startIndex.advancedBy(range.location )
-		return Range( start: startIndex, end: startIndex.advancedBy(range.length ))
-	}
-	
-	/// Не учтиывает спецсимволы юникода
-	func NSRangeFromRange( range: Range<String.Index> ) -> NSRange {
-		let start = self.startIndex.distanceTo(range.startIndex)
-		let length = range.startIndex.distanceTo(range.endIndex)
-		return NSMakeRange( start, length )
-	}
 	
 	func stringByReplacingCharactersInRange( range: NSRange, withString replacement: String ) -> String {
-		var newString = self
-		newString.replaceRange( newString.rangeFromNSRange( range ), with: replacement )
-		return newString
+		let newString = self as NSString
+		return newString.stringByReplacingCharactersInRange( range, withString: replacement )
 	}
 	
 	var asPhoneNumber: String {
-		let tendigits = substringFromIndex( endIndex.advancedBy(-10 ))
-		let city = tendigits.substringWithRange( rangeFromNSRange( NSMakeRange( 0, 3 )))
-		let triad = tendigits.substringWithRange( rangeFromNSRange( NSMakeRange( 3, 3 )))
-		let firstpair = tendigits.substringWithRange( rangeFromNSRange( NSMakeRange( 6, 2 )))
-		let secondpair = tendigits.substringWithRange( rangeFromNSRange( NSMakeRange( 8, 2 )))
+		
+		guard length >= 10 else { return self }
+		
+		let tendigits = substringFromIndex( endIndex.advancedBy( -10 ))
+		let index = tendigits.startIndex
+		let city = tendigits.substringToIndex( index + 3 )
+		let triad = tendigits.substringWithRange( index + 3..<index + 6 )
+		let firstpair = tendigits.substringWithRange( index + 6..<index + 8 )
+		let secondpair = tendigits.substringWithRange( index + 8..<index + 10 )
 		
 		return "+7 \( city ) \( triad ) \( firstpair ) \( secondpair )"
 	}
 	
 	var digitsOnly: String {
 		get {
-			let range = Range<String.Index>( start: startIndex, end: endIndex )
+			let range = startIndex..<endIndex
 			return stringByReplacingOccurrencesOfString("[^0-9]", withString: "", options: NSStringCompareOptions.RegularExpressionSearch, range: range )
 		}
 	}
@@ -56,9 +47,14 @@ extension String {
 			return number.substringFromIndex( number.startIndex.advancedBy(index ) )
 		}
 	}
-
+	
 	var rub: String { return self + "₽" }
 }
+
+private func +<T: ForwardIndexType>( lhs: T, rhs: T.Distance )->T {
+	return lhs.advancedBy( rhs )
+}
+
 
 
 /**
