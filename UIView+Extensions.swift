d5dc46273af36f2a898bce366ac864fc8fe4ed65
Vladimir Kazantsev <kazantsev@mc2soft.ru>
2017-03-25 00:12:00 +0300

    Clearer naming of UIVIew extension.

--- UIView+Extensions.swift
+++ UIView+Extensions.swift
@@ -10,39 +10,29 @@ import UIKit
 extension UIView {
 
 	/**
-	Load xib in memory
-	
-	- parameter xib:    Name of the xib
-	- parameter ofType: View type to search in xib. Ex: MyViewClass.self
-	
+	Load xib in memory.
+
+	- parameter xib:    Name of the xib. Default to type name.
 	- returns: Instance of ofType class, loaded from xib
 	*/
-	class func viewFromXib( _ xib: String? = nil ) -> Self? {
+	class func makeFromXib( named xibName: String? = nil ) -> Self? {
 
-		let xibFile = xib ?? NSStringFromClass( self as! AnyClass ).components( separatedBy: "." ).last!
+		guard let xibFile = xibName ??
+			NSStringFromClass( self ).components( separatedBy: "." ).last
+			else {
+				return nil
+		}
 
 		let nib = UINib( nibName: xibFile, bundle: nil )
-		let views = nib.instantiate( withOwner: nil, options: nil )
-		
-		for anyObject in views {
-			if type(of: (anyObject) as AnyObject) === self {
-				return helperConvertObject( anyObject as AnyObject, type: self )
-			}
-		}
-	
-		return nil
+		let views = nib.instantiate( withOwner: nil, options: nil ) as [ AnyObject ]
+
+		guard let selfObject = views.first( where : { type( of: $0 ) === self } )
+			else { return nil }
+
+		return helperConvertObject( selfObject, type: self )
 	}
-	
-	fileprivate class func helperConvertObject<T>( _ object: AnyObject, type: T.Type ) -> T? {
+
+	private class func helperConvertObject<T>( _ object: AnyObject, type: T.Type ) -> T? {
 		return object as? T
-	}
-
-}
-
-extension UIView {
-	///
-	convenience init( useAutolayout: Bool ) {
-		self.init( frame: .zero )
-		translatesAutoresizingMaskIntoConstraints = useAutolayout
 	}
 }
