fc6b178ac388bee7ce515b38c3097521a06655d3
Vladimir Kazantsev <8406740@gmail.com>
2018-09-07 10:52:21 +0300

    Updated CG Extensions.

--- Source/CoreGraphics+Extensions.swift
+++ Source/CoreGraphics+Extensions.swift
@@ -10,6 +10,25 @@ import CoreGraphics
 
 public extension CGRect {
 
+	static func *= ( rect: inout CGRect, multiplier: CGFloat ) {
+		rect = CGRect( x: rect.origin.x * multiplier,
+					   y: rect.origin.y * multiplier,
+					   width: rect.size.width * multiplier,
+					   height: rect.size.height * multiplier )
+	}
+}
+
+
+public extension CGRect {
+
+	init( square side: CGFloat ) {
+		self.init( x: 0, y: 0, width: side, height: side )
+	}
+
+	init( square side: Int ) {
+		self.init( x: 0, y: 0, width: side, height: side )
+	}
+
 	/// Center of rect property.
 	var center: CGPoint {
 		get { return CGPoint( x: midX, y: midY ) }
@@ -27,4 +46,19 @@ public extension UIEdgeInsets {
 	public init( horizontal: CGFloat = 0, vertical: CGFloat = 0 ) {
 		self.init( top: vertical, left: horizontal, bottom: vertical, right: horizontal )
 	}
+}
+
+public extension Int {
+
+	/// Returns radians value of the receivers degrees.
+	var radians: CGFloat { return .pi * CGFloat( self ) / 180 }
+}
+
+public extension CGFloat {
+
+	/// Returns radians value of the receivers degrees.
+	var radians: CGFloat { return .pi * self / 180 }
+
+	/// Returns degree value of the receivers radians.
+	var degrees: CGFloat { return self / .pi * 180 }
 }
