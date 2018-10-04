ce3b006d3bc0f1a3f2fe1d1f0d7a2b489d3c9ffe
Vladimir Kazantsev <8406740@gmail.com>
2018-10-04 13:15:51 +0300

    Methods for navigating UINavigationController with completion handlers are now always animated.

--- Source/UI/UINavigationController+Blocks.swift
+++ Source/UI/UINavigationController+Blocks.swift
@@ -78,31 +78,74 @@ public extension UIViewController {
 }
 
 public extension UINavigationController {
-	
-	func pushViewController( _ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void ) {
+
+	/// Pushes a view controller onto the receiver’s stack and updates the display.
+	/// Calls `completion` handler after transition animation is over.
+	/// - parameter viewController: The view controller to push onto the stack.
+	/// This object cannot be a tab bar controller.
+	/// If the view controller is already on the navigation stack, this method throws an exception.
+	/// - parameter completion: Handler that will be called after transition animation is finished.
+	/// - note: Transition is always animated because there is no point in completion handler otherwise.
+	func pushViewController( _ viewController: UIViewController, completion: @escaping () -> Void ) {
 			
-		pushViewController( viewController, animated: animated )
+		pushViewController( viewController, animated: true )
 		setCompletionHandler( completion )
 	}
 
-	func popViewController( animated: Bool, completion: @escaping () -> Void ) {
+	/// Pops the top view controller from the navigation stack and updates the display.
+	/// Calls `completion` handler after transition animation is over.
+	/// - parameter completion: Handler that will be called after transition animation is finished.
+	/// - returns: The view controller that was popped from the stack.
+	/// - note: Transition is always animated because there is no point in completion handler otherwise.
+	@discardableResult
+	func popViewController( completion: @escaping () -> Void ) -> UIViewController? {
 		
-		popViewController( animated: animated )
+		let controller = popViewController( animated: true )
 		setCompletionHandler( completion )
+		return controller
 	}
 
-	func popToViewController( _ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void ) {
+	/// Pops view controllers until the specified view controller is at the top of the navigation stack.
+	/// Calls `completion` handler after transition animation is over.
+	/// - parameter viewController: The view controller that you want to be at the top of the stack.
+	/// This view controller must currently be on the navigation stack.
+	/// - parameter completion: Handler that will be called after transition animation is finished.
+	/// - returns: An array containing the view controllers that were popped from the stack.
+	/// - note: Transition is always animated because there is no point in completion handler otherwise.
+	/// - note: Completion handler may be called instantly if viewController is already at the top of the stack.
+	@discardableResult
+	func popToViewController( _ viewController: UIViewController, completion: @escaping () -> Void ) -> [ UIViewController ]? {
 
-		popToViewController( viewController, animated: animated )
+		guard topViewController != viewController else {
+			completion()
+			return nil
+		}
+
+		let controllers = popToViewController( viewController, animated: true )
 		setCompletionHandler( completion )
+		return controllers
+	}
+
+	/// Pops all the view controllers on the stack except the root view controller and updates the display.
+	/// Calls `completion` handler after transition animation is over.
+	/// - parameter completion: Handler that will be called after transition animation is finished.
+	/// - returns: An array of view controllers representing the items that were popped from the stack.
+	/// - note: Transition is always animated because there is no point in completion handler otherwise.
+	/// - note: Completion handler may be called instantly if navigation top controller is root controller.
+	@discardableResult
+	func popToRootViewController( completion: @escaping () -> Void ) -> [ UIViewController ]? {
+		
+		guard viewControllers.count > 1 else {
+			completion()
+			return nil
+		}
+
+		let controllers = popToRootViewController( animated: true )
+		setCompletionHandler( completion )
+		return controllers
 	}
 	
-	func popToRootViewController( animated: Bool, completion: @escaping () -> Void ) {
-		
-		popToRootViewController( animated: animated )
-		setCompletionHandler( completion )
-	}
-	
+
 	private func setCompletionHandler( _ completion: @escaping () -> Void ) {
 		if let coordinator = transitionCoordinator {
 			coordinator.animate( alongsideTransition: nil ) { _ in
