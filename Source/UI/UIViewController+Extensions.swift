87c5c49432a5cad5f7b33b873c16a5ae5f2ad72b
Vladimir Kazantsev <8406740@gmail.com>
2018-11-29 11:31:27 +0300

    Convenience properties for distances to safe areas.

---
+++ Source/UI/UIViewController+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Extensions.swift
+//
+//  Created by Vladimir on 29/11/2018.
+//  Copyright © 2018 Vladimir Kazantsev. All rights reserved.
+//
+
+import UIKit
+
+public extension UIViewController {
+
+	/// Returns distance from top to safe area. If running on iOS versions prior to 11,
+	/// uses `topLayoutGuide` length instead.
+	public var topSafeAreaInset: CGFloat {
+		guard #available( iOS 11, * ) else { return topLayoutGuide.length }
+		return view.safeAreaInsets.top
+	}
+
+	/// Returns distance from bottom to safe area. If running on iOS versions prior to 11,
+	/// uses `bottomLayoutGuide` length instead.
+	public var bottomSafeAreaInset: CGFloat {
+		guard #available( iOS 11, * ) else { return bottomLayoutGuide.length }
+		return view.safeAreaInsets.bottom
+	}
+}
