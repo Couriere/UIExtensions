a5119cb3a1bfa835c5291693ba32a2ad4b530d0a
Vladimir Kazantsev <8406740@gmail.com>
2018-12-05 11:53:51 +0300

    Updated UIImage extension.

--- Source/UI/UIImage+Extensions.swift
+++ Source/UI/UIImage+Extensions.swift
@@ -9,18 +9,49 @@ import UIKit
 
 public extension UIImage {
 
+	/// Initializes and returns an image object of specified size, filled with specified color.
 	convenience init( size: CGSize = CGSize( width: 1, height: 1 ), color: UIColor ) {
-		let rect = CGRect( x: 0, y: 0, width: size.width, height: size.height )
-		UIGraphicsBeginImageContextWithOptions( rect.size, false, 0 );
+
+		let rect = CGRect( origin: .zero, size: size )
+
+		UIGraphicsBeginImageContextWithOptions( rect.size, false, 0 )
+		defer { UIGraphicsEndImageContext() }
 		color.setFill()
 		UIRectFill( rect )
-		self.init( cgImage: UIGraphicsGetImageFromCurrentImageContext()!.cgImage!, scale: UIScreen.main.scale, orientation: .up )
-		UIGraphicsEndImageContext()
+
+		self.init( cgImage: UIGraphicsGetImageFromCurrentImageContext()!.cgImage!,
+				   scale: UIScreen.main.scale,
+				   orientation: .up )
 	}
 }
 
 
 public extension UIImage {
+
+	/// Changes length of bigger side of the image to provided value.
+	/// Maintains aspect ratio of the image.
+	func scaled( toSideLenght side: CGFloat ) -> UIImage {
+
+		let aspectRatio = size.width / size.height
+		let scaledSize: CGSize = aspectRatio > 1 ?
+			CGSize( width: side, height: side / aspectRatio ) :
+			CGSize( width: side * aspectRatio, height: side )
+
+		if #available( iOS 10, * ) {
+			let format: UIGraphicsImageRendererFormat = .default()
+			format.scale = self.scale
+			return UIGraphicsImageRenderer( size: scaledSize, format: format )
+				.image { context in
+					draw( in: context.format.bounds )
+			}
+		} else {
+
+			UIGraphicsBeginImageContextWithOptions( scaledSize, false, self.scale )
+			defer { UIGraphicsEndImageContext() }
+			draw( in: CGRect( origin: .zero, size: scaledSize ))
+			return UIGraphicsGetImageFromCurrentImageContext() ?? self
+		}
+	}
 
 	//////
 	/// Apply `color` to all non-transparent pixels in image.
@@ -29,12 +60,10 @@ public extension UIImage {
 
 		let image = withRenderingMode( .alwaysTemplate )
 
-		UIGraphicsBeginImageContextWithOptions( size, false, scale )
+		UIGraphicsBeginImageContextWithOptions( size, false, 0 )
+		defer { UIGraphicsEndImageContext() }
 		color.set()
-		image.draw( in: CGRect( x: 0, y: 0, width: size.width, height: size.height ))
-		let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
-		UIGraphicsEndImageContext()
-
-		return tintedImage
+		image.draw( in: CGRect( origin: .zero, size: size ))
+		return UIGraphicsGetImageFromCurrentImageContext()
 	}
 }
