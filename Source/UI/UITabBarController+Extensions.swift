e93acbbc75f6a05af0b50380451db35726ba3925
Vladimir Kazantsev <8406740@gmail.com>
2018-12-05 10:13:46 +0300

    Added extension to hide/show tab bar.

---
+++ Source/UI/UITabBarController+Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  UITabBarController+Extensions.swift
+//  UIExtensions
+//
+//  Created by Vladimir on 30/09/2018.
+//  Copyright © 2018. All rights reserved.
+//
+
+import UIKit
+
+public extension UITabBarController {
+
+	/// Shows/hides tab bar in UITabBarController.
+	/// Correctly changes safe area zone in subviews.
+	public var isTabBarHidden: Bool {
+		get { return !tabBar.frame.intersects( view.frame ) }
+		set {
+			guard isTabBarHidden != newValue else { return }
+
+			let offsetY = newValue ? tabBar.frame.height : -tabBar.frame.height
+			let endFrame = tabBar.frame.offsetBy( dx: 0, dy: offsetY )
+
+			// Update safe area insets for the current view controller.
+			if #available( iOS 11, * ),
+				let childController = viewControllers?[ selectedIndex ] {
+
+				var newInsets = childController.additionalSafeAreaInsets
+				newInsets.bottom += -offsetY
+
+				childController.additionalSafeAreaInsets = newInsets
+				childController.view.setNeedsLayout()
+			}
+
+			tabBar.frame = endFrame
+		}
+	}
+}
