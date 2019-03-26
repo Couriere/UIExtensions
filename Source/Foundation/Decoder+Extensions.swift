ab59365a1c12a3f4283686948378a327bb8cf69d
Vladimir Kazantsev <8406740@gmail.com>
2019-03-26 11:11:01 +0300

    Added method for decoding unkeyed container without throwing an error if decoding fails.

---
+++ Source/Foundation/Decoder+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Decoder+Extensions.swift
+//  UIExtensions iOS
+//
+//  Created by Vladimir on 26/03/2019.
+//  Copyright © 2019 Vladimir Kazantsev. All rights reserved.
+//
+
+import Foundation
+
+private struct DecoderDummyEmptyValue: Decodable {}
+
+public extension UnkeyedDecodingContainer {
+
+	/// Iterates over unkeyed container elements trying to decode each element.
+	/// If element decoding fails, method skips it and continiues to the next.
+	mutating func compactDecode<T: Decodable>() -> [ T ] {
+
+		var result: [ T ] = []
+		while !self.isAtEnd {
+			if let value = try? self.decode( T.self ) {
+				result.append( value )
+			} else {
+				_ = try? self.decode( DecoderDummyEmptyValue.self )
+			}
+		}
+		return result
+	}
+}
