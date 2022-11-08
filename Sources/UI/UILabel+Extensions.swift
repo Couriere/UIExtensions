ae9df8ebd5dc51c3b60eb7b923b8eaf7bd3902bf
Vladimir Kazantsev <8406740@gmail.com>
2022-11-08 12:01:12 +0530

    SwiftUI-like text formatting in UILabel.

--- Sources/UI/UILabel+Extensions.swift
+++ Sources/UI/UILabel+Extensions.swift
@@ -27,9 +27,10 @@ public extension UILabel {
 
 	///  Initialize UILabel and setting it's attributedText property with parameter.
 	/// - parameters attributedText: Text to set after initialization.
-	convenience init( _ attributedText: NSAttributedString ) {
+	convenience init( _ attributedText: NSAttributedString? ) {
 		self.init( frame: .zero )
 		self.attributedText = attributedText
+		self.isHidden = attributedText == nil
 		self.numberOfLines = 0
 	}
 }
@@ -64,6 +65,124 @@ public extension UILabel {
 			$0.duration = duration
 		}
 		layer.add( animation, forKey: CATransitionType.fade.rawValue )
+	}
+}
+
+
+public extension UILabel {
+
+	///  Initialize UILabel and setting it's text property with parameter.
+	/// - parameters text: Text to set after initialization.
+	convenience init( _ text: String? ) {
+		self.init( frame: .zero )
+		self.text = text
+		self.isHidden = text == nil
+		self.numberOfLines = 0
+	}
+
+	/// Changes font of the text in the `UILabel`.
+	func font( _ uiFont: UIFont ) -> Self {
+		attributedText = attributedText?.font( uiFont )
+		return self
+	}
+
+	/// Changes color of the text in the `UILabel`.
+	func color( _ uiColor: UIColor ) -> Self {
+		attributedText = attributedText?.color( uiColor )
+		return self
+	}
+
+	/// Sets `kern` attribute of the text in the `UILabel`.
+	func kern( _ kern: CGFloat ) -> Self {
+		attributedText = attributedText?.kern( kern )
+		return self
+	}
+
+	/// Sets `baselineOffset` attribute of the text in the `UILabel`.
+	func baselineOffset( _ offset: CGFloat ) -> Self {
+		attributedText = attributedText?.baselineOffset( offset )
+		return self
+	}
+
+	/// Sets `strikethroughStyle` attribute of the text in the `UILabel`.
+	func strikethroughStyle( _ style: NSUnderlineStyle, color: UIColor = .black ) -> Self {
+		attributedText = attributedText?.strikethroughStyle( style, color: color )
+		return self
+	}
+
+	/// Sets `underlineStyle` attribute of the text in the `UILabel`.
+	func underlineStyle( _ style: NSUnderlineStyle, color: UIColor = .black ) -> Self {
+		attributedText = attributedText?.underlineStyle( style, color: color )
+		return self
+	}
+
+	/// Sets `paragraphStyle` attribute of the text in the `UILabel`.
+	/// - note: All existing paragraph styles will be overwritten.
+	func paragraphStyle( _ paragraphStyle: NSParagraphStyle ) -> Self {
+		attributedText = attributedText?.paragraphStyle( paragraphStyle )
+		return self
+	}
+
+	/// Sets `lineSpacing` value in `paragraphStyle` attribute
+	/// of the text in the `UILabel`.
+	func lineSpacing( _ lineSpacing: CGFloat ) -> Self {
+		attributedText = attributedText?.lineSpacing( lineSpacing )
+		return self
+	}
+
+	/// Sets `paragraphSpacing` value in `paragraphStyle` attribute
+	/// of the text in the `UILabel`.
+	func paragraphSpacing( _ paragraphSpacing: CGFloat ) -> Self {
+		attributedText = attributedText?.paragraphSpacing( paragraphSpacing )
+		return self
+	}
+
+	/// Sets `lineHeightMultiple` value in `paragraphStyle` attribute
+	/// of the text in the `UILabel`.
+	func lineHeightMultiple( _ multiple: CGFloat ) -> Self {
+		attributedText = attributedText?.lineHeightMultiple( multiple )
+		return self
+	}
+
+	/// Sets `minimumLineHeight` value in `paragraphStyle` attribute
+	/// of the text in the `UILabel`.
+	func minimumLineHeight( _ minimumLineHeight: CGFloat ) -> Self {
+		attributedText = attributedText?.minimumLineHeight( minimumLineHeight )
+		return self
+	}
+
+	/// Sets `alignment` value in `paragraphStyle` attribute
+	/// of the text in the `UILabel`.
+	func alignment( _ alignment: NSTextAlignment ) -> Self {
+		attributedText = attributedText?.alignment( alignment )
+		return self
+	}
+
+	/// Sets `lineBreakMode` value in `paragraphStyle` attribute
+	/// of the text in the `UILabel`.
+	func lineBreakMode( _ lineBreakMode: NSLineBreakMode ) -> Self {
+		attributedText = attributedText?.lineBreakMode( lineBreakMode )
+		return self
+	}
+
+	/// Sets `paragraphStyle` attribute of the text in the `UILabel`.
+	/// - note: All existing paragraph styles will be overwritten.
+	func image(
+		_ image: UIImage,
+		at location: Int? = nil,
+		verticalOffset: CGFloat = 0
+	) -> Self {
+		attributedText = attributedText?
+			.image( image, at: location, verticalOffset: verticalOffset )
+		return self
+	}
+
+
+	/// Sets attributes of the text in the `UILabel`.
+	/// - note: All existing attributes will be overwritten.
+	func attributes( _ attributes: [NSAttributedString.Key: Any] ) -> Self {
+		attributedText = text?.attributes( attributes )
+		return self
 	}
 }
 
