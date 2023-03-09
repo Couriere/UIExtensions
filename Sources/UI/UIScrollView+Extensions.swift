9a06da4c9fa93545dd8b1ce7e6a01259ec09f7ee
Vladimir Kazantsev <8406740@gmail.com>
2023-03-09 14:51:46 +0300

    Convenience inits for UIScrollView.

--- Sources/UI/UIScrollView+Extensions.swift
+++ Sources/UI/UIScrollView+Extensions.swift
@@ -25,10 +25,98 @@ import UIKit
 
 public extension UIScrollView {
 
+	/// Creates a new UIScrollView with a content view containing multiple subviews.
+	///
+	/// - Parameters:
+	///   - showIndicators: A Boolean value that determines whether or not
+	/// the scroll view should display scroll indicators.
+	///   - content: A closure that returns an array of UIViews that
+	/// will be used as the content view's subviews.
+	///
+	/// - Returns: A new UIScrollView with the specified content view.
+	///
+	/// This convenience initializer simplifies the process of creating
+	/// a scroll view with a content view containing multiple subviews.
+	/// It takes care of setting up the scroll view's properties
+	/// and adding the content view as a subview, and automatically lays
+	/// out the content view to fill the scroll view.
+	///
+	/// - note: Scroll view subviews are pinned to frame layout guide horizontally
+	/// and to content layout guide vertically.
+	///
+	/// If the array of subviews returned by the `content` closure
+	/// contains only one view, that view is used as the content view.
+	/// Otherwise, a vertical UIStackView is created with the subviews
+	/// as arranged subviews.
+	///
+	/// Example usage:
+	/// ```
+	/// let scrollView = UIScrollView {
+	///   [
+	///     UILabel(),
+	///     UIImageView(),
+	///     UIButton(),
+	///   ]
+	/// }
+	/// ```
+	///
+	/// or:
+	/// ```
+	/// let scrollView = UIScrollView {
+	///     UIStackView( .horizontal ) {
+	///         UILabel()
+	///         UIImageView()
+	///         UIButton()
+	///     }
+	/// }
+	/// ```
+	convenience init( showIndicators: Bool = true, _ content: () -> [ UIView ] ) {
+
+		self.init( frame: .zero )
+
+		translatesAutoresizingMaskIntoConstraints = false
+		showsVerticalScrollIndicator = showIndicators
+		showsHorizontalScrollIndicator = showIndicators
+
+		let innerView: UIView
+
+		let builtViews = content()
+		precondition( builtViews.isNotEmpty )
+
+		if builtViews.count == 1 {
+			innerView = builtViews[0]
+		} else {
+			innerView = UIStackView { builtViews }
+		}
+		
+		addSubview( innerView )
+		innerView.translatesAutoresizingMaskIntoConstraints = false
+
+		innerView.pin( .horizontal, to: frameLayoutGuide )
+		innerView.pin( .vertical, to: contentLayoutGuide )
+	}
+}
+
+public extension UIScrollView {
+
+	/// Constrains the width of the content inside the scroll view
+	/// to be equal to the width of the scroll view's frame,
+	/// with an optional constant value added to the width.
+	///
+	/// - parameter constant: The constant value to add to the width of the content.
+	/// - returns: The layout constraint that was activated.
+	///
+	/// This method is useful when you want the content to stay centered
+	/// in the scroll view horizontally, regardless of the size
+	/// of the scroll view. To achieve this, the content inset
+	/// of the scroll view is adjusted, effectively centering the content.
+	///
 	@discardableResult
 	func constrainContentWidthToFrame( constant: CGFloat = 0 ) -> NSLayoutConstraint {
-		let constraint = contentLayoutGuide.widthAnchor.constraint( equalTo: frameLayoutGuide.widthAnchor,
-																	constant: constant )
+		let constraint = contentLayoutGuide.widthAnchor.constraint(
+			equalTo: frameLayoutGuide.widthAnchor,
+			constant: constant
+		)
 		constraint.isActive = true
 		contentInset = UIEdgeInsets( horizontal: -constant / 2, vertical: 0 )
 		return constraint
