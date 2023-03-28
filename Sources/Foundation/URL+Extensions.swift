fa16bc28c0d7667d086b92b40779223e9f961d7e
Vladimir Kazantsev <8406740@gmail.com>
2023-03-28 12:47:01 +0300

    Added new URL initialization method.

--- Sources/Foundation/URL+Extensions.swift
+++ Sources/Foundation/URL+Extensions.swift
@@ -24,6 +24,21 @@ import Foundation
 
 public extension URL {
 
+	/// Initializes a new URL with the provided `string`.
+	///	- parameter string: A `StaticString` representation of the URL.
+	///	- returns: A newly initialized `URL` object.
+	init( _ string: StaticString ) {
+		let string = string.withUTF8Buffer {
+			String( decoding: $0, as: UTF8.self )
+		}
+		self.init( string: string )!
+	}
+}
+
+public extension URL {
+
+	/// Returns a new URL with the query component removed.
+	/// - returns: A new `URL` object with the query component removed.
 	var urlByDeletingQuery: URL? {
 		var components = URLComponents( url: self, resolvingAgainstBaseURL: false )
 		components?.query = nil
