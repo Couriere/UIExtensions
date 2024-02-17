759e8dbe2d9410cb35661e0f144aab6d6257a228
Vladimir Kazantsev <8406740@gmail.com>
2024-02-17 16:10:55 +0300

    `.onChange` modifier for async actions expanded.

--- Sources/SwiftUI/View+OnChangeAsync.swift
+++ Sources/SwiftUI/View+OnChangeAsync.swift
@@ -37,8 +37,7 @@ public extension View {
 	/// If `initial` parameter is true, this modifier perform an asynchronous
 	/// task with a lifetime that matches that of the modified view.
 	///
-	/// SwiftUI passes the new value into the closure. You can also capture the
-	/// previous value to compare it to the new value.
+	/// The old and new observed values are passed into the closure.
 	///
 	/// - Parameters:
 	///   - value: The value to check against when determining whether
@@ -57,7 +56,7 @@ public extension View {
 		of value: V,
 		initial: Bool = false,
 		priority: TaskPriority = .userInitiated,
-		perform action: @escaping (_ newValue: V) async -> Void
+		_ action: @escaping (_ oldValue: V, _ newValue: V) async -> Void
 	) -> some View where V : Equatable {
 
 		return self
@@ -83,9 +82,6 @@ public extension View {
 	/// If `initial` parameter is true, this modifier perform an asynchronous
 	/// task with a lifetime that matches that of the modified view.
 	///
-	/// SwiftUI passes the new value into the closure. You can also capture the
-	/// previous value to compare it to the new value.
-	///
 	/// - Parameters:
 	///   - value: The value to check against when determining whether
 	///     to run the closure.
@@ -103,7 +99,7 @@ public extension View {
 		of value: V,
 		initial: Bool = false,
 		priority: TaskPriority = .userInitiated,
-		perform action: @escaping () async -> Void
+		_ action: @escaping () async -> Void
 	) -> some View where V : Equatable {
 
 		return self
@@ -112,42 +108,111 @@ public extension View {
 					value: value,
 					initial: initial,
 					priority: priority,
-					action: { _ in await action() }
+					action: { _, _ in await action() }
 				)
 			)
 	}
 }
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
-public struct OnChangeAsyncModifier<V: Equatable>: ViewModifier {
+public extension View {
+
+	/// Adds a modifier for this view that fires an asynchronous action when a specific
+	/// value changes.
+	///
+	/// You can use `onChange` to trigger a side effect as the result of a
+	/// value changing, such as an `Environment` key or a `Binding`.
+	///
+	/// If the task doesn't finish before SwiftUI removes the view
+	/// or the view changes identity, SwiftUI cancels the task.
+	///
+	/// If `initial` parameter is true, this modifier perform an asynchronous
+	/// task with a lifetime that matches that of the modified view.
+	///
+	/// SwiftUI passes the new value into the closure.
+	///
+	/// - Parameters:
+	///   - value: The value to check against when determining whether
+	///     to run the closure.
+	///   - initial: Whether the action should be run when this view initially
+	///     appears.
+	///   - priority: The priority of the asynchronous task.
+	///   - action: An asynchronous closure to run when the value changes.
+	///
+	/// - Note: If the value changes while a previous asynchronous task is still running,
+	///   the previous task will be cancelled, and the new task
+	///   will be executed with the most recent value.
+	///
+	/// - Returns: A view that fires an asynchronous action when the specified value changes.
+	@available(iOS, deprecated: 17.0, message: "Use `onChange` with a two or zero parameter action closure instead.")
+	@available(macOS, deprecated: 14.0, message: "Use `onChange` with a two or zero parameter action closure instead.")
+	@available(tvOS, deprecated: 17.0, message: "Use `onChange` with a two or zero parameter action closure instead.")
+	@available(watchOS, deprecated: 10.0, message: "Use `onChange` with a two or zero parameter action closure instead.")
+	func onChange<V>(
+		of value: V,
+		initial: Bool = false,
+		priority: TaskPriority = .userInitiated,
+		perform action: @escaping (_ newValue: V) async -> Void
+	) -> some View where V : Equatable {
+
+		return self
+			.modifier(
+				OnChangeAsyncModifier(
+					value: value,
+					initial: initial,
+					priority: priority,
+					action: { _, newValue in await action( newValue ) }
+				)
+			)
+	}
+}
+
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+public struct OnChangeAsyncModifier<V>: ViewModifier where V: Equatable, V: Sendable {
 
 	public let value: V
 	public let initial: Bool
 	public let priority: TaskPriority
-	public let action: ( V ) async -> Void
+	public let action: ( _ oldValue: V, _ newValue: V ) async -> Void
 
 	@State private var task: Task<Void, Never>?
 
 	public func body( content: Content ) -> some View {
 
-		content
-			.onChange( of: value ) { value in
-				task?.cancel()
-				task = Task( priority: priority ) {
-					await action( value )
+		if #available( iOS 17, tvOS 17, macOS 14, watchOS 10, * ) {
+			content
+				.onChange( of: value, initial: initial ) { oldValue, newValue in
+					task?.cancel()
+					task = Task( priority: priority ) {
+						await action( oldValue, newValue )
+					}
 				}
-			}
-			.onAppear {
-				guard initial else { return }
-				
-				task?.cancel()
-				task = Task( priority: priority ) {
-					await action( value )
+				.onDisappear {
+					task?.cancel()
+					task = nil
 				}
-			}
-			.onDisappear {
-				task?.cancel()
-				task = nil
-			}
+		}
+		else {
+
+			content
+				.onChange( of: value ) { [oldValue=value] newValue in
+					task?.cancel()
+					task = Task( priority: priority ) {
+						await action( oldValue, newValue )
+					}
+				}
+				.onAppear {
+					guard initial else { return }
+
+					task?.cancel()
+					task = Task( priority: priority ) {
+						await action( value, value )
+					}
+				}
+				.onDisappear {
+					task?.cancel()
+					task = nil
+				}
+		}
 	}
 }
