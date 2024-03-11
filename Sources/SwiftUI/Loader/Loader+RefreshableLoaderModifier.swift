ed9cd57946f722a0b7d736f51409d7ec055c7a81
Vladimir Kazantsev <8406740@gmail.com>
2024-03-11 13:56:22 +0300

    Typos...

--- Sources/SwiftUI/Loader/Loader+RefreshableLoaderModifier.swift
+++ Sources/SwiftUI/Loader/Loader+RefreshableLoaderModifier.swift
@@ -87,7 +87,7 @@ internal extension EnvironmentValues {
 	}
 }
 
-@available(iOS 14.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 internal struct TaskWrapper {
 
 	let action: () async -> Void
